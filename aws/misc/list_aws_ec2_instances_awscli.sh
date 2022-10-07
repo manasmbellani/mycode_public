ee700ddef10bd9af326e0b5f37f1789bad9ab5f9
manasbellani <manasmbellani@gmail.com>
2022-10-07 23:38:59 +1100

    list_aws_ec2_instances_awscli.sh: added state, public IP for ec2 instances

--- aws/misc/list_aws_ec2_instances_awscli.sh
+++ aws/misc/list_aws_ec2_instances_awscli.sh
@@ -31,8 +31,10 @@ for i in $(seq 0 $instances_count); do
     # Get instance ID
     instance_id=$(echo "$instances_out" | jq -r ".Reservations[$i].Instances[0].InstanceId")
 
-    # Get the instance tags 
+    # Get the instance ID
     if [ ! -z "$instance_id" ] && [ "$instance_id" != "null" ]; then
+        
+        # Get the instance tags 
         instance_tags=$(echo "$instances_out" | jq -r ".Reservations[$i].Instances[0].Tags")
         instance_tags_count=$(echo "$instances_out" | jq -r ".Reservations[$i].Instances[0].Tags | length")
 
@@ -45,9 +47,14 @@ for i in $(seq 0 $instances_count); do
                 instance_name="$instance_tag_value"
             fi
         done
+
+        # Get the instance's state
+        instance_state=$(echo "$instances_out" | jq -r ".Reservations[$i].Instances[0].State.Name")
+
+        # Get the public IP
+        public_ip=$(echo "$instances_out" | jq -r ".Reservations[$i].Instances[0].PublicIpAddress")
+
         # Output the instance ID and name
-        echo "$instance_id$DELIM$instance_name"
+        echo "$instance_id$DELIM$instance_name$DELIM$instance_state$DELIM$public_ip"
     fi
 done
-
-
