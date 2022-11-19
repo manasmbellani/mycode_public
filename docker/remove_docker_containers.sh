75bae0314876a2aa3369e3d1b072ee4aef38b755
manasbellani <manasmbellani@gmail.com>
2022-11-19 23:33:06 +1100

    remove_docker_containers.sh: updated code for script to update docker container

--- docker/remove_docker_containers.sh
+++ docker/remove_docker_containers.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+USAGE="[-]
+Usage:
+    $0 <image_name>
+Summary:
+    Remove image
+"
+if [ $# -lt 1 ]; then
+    echo "$USAGE"
+    exit 1
+fi
+image_name="$1"
+
+echo "[*] Removing container with image: $image_name"
+docker container ls -a \
+    | grep -i "$image_name" \
+    | grep -iv "CONTAINER ID" \
+    | cut -d " " -f1 \
+    | xargs -I ARG docker rm ARG
