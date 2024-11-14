d483983c057b0f1291dc2340c37acf4e96a21576
manasbellani <manasmbellani@gmail.com>
2024-11-14 18:12:17 +1100

    main.sh: added printable comments to commands as they execute

--- invoke_macbook_videos_backup/main.sh
+++ invoke_macbook_videos_backup/main.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "[*] Recreating videos folder..."
 rm -rf ~/Downloads/videos 2>/dev/null; mkdir ~/Downloads/videos 2>/dev/null
+
+echo "[*] Copying videos to the ~/Downloads/videos folder..."
 find ~/Pictures/Photos\ Library.photoslibrary/originals -ipath "*.mov" | xargs -I ARG cp ARG ~/Downloads/videos
 cd ~/Downloads/videos
+
+echo "[*] Deleting copied videos..."
+find ~/Pictures/Photos\ Library.photoslibrary/originals -ipath "*.mov" | xargs -I ARG rm ARG
