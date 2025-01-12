d535f81066f1a6ae6aecd946d2010f3871ff6745
manasbellani <manasmbellani@gmail.com>
2025-01-13 10:56:47 +1100

    get_cve_vuln_info: added support for 'EPSS' and Nomi-Sec's 'POCs in Github' repositories

--- get_cve_vuln_info/README.md
+++ get_cve_vuln_info/README.md
@@ -2,12 +2,14 @@
 
 Script to get the information about a given CVE / vulnerability
 
-Script will pull information from `CveListv5` CVE database about a CVE, and various sources such as CISA KEV, EPSS.
+Script will pull information from `CveListv5` CVE database about a CVE, and various sources such as CISA KEV, EPSS. The information is pulled by either cloning the repository where information is stored locally (eg CVEListv5, CISA Kev) or via the API (eg EPSS)
 
-Currently, following sources are supported:
+Currently, following sources are searched for CVE Info:
 - CVEListv5
 - EPSS
 - CISA KEV
+- ExploitDB
+- Nomi-Sec's PoC in Github
 
 ## Setup
 
--- get_cve_vuln_info/main.sh
+++ get_cve_vuln_info/main.sh
@@ -3,12 +3,16 @@ SCRIPT_DIR=$(dirname "$0")
 USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
 CVELIST_CLONE_DIR="$HOME/opt/cvelistV5"
 CVELIST_URL="https://github.com/CVEProject/cvelistV5"
+EXPLOITDB_CLONE_DIR="$HOME/opt/exploitdb"
+EXPLOITDB_CLONE_URL="https://gitlab.com/exploit-database/exploitdb.git/"
+POCS_IN_GITHUB_CLONE_DIR="$HOME/opt/PoC-in-GitHub"
+POCS_IN_GITHUB_CLONE_URL="https://github.com/nomi-sec/PoC-in-GitHub"
 CVEDB_FILE_PREFIX="cvedb"
 WRITE_TO_OUTFILE=1
 OUTFILE_PREFIX="out-cve-info"
 TMP_DIR="/tmp"
 
-USAGE="$0 <cve_id> [write_to_outfile=$WRITE_TO_OUTFILE] [outfile_prefix=$SCRIPT_DIR/$OUTFILE_PREFIX] [cvelist_clone_dir=$CVELIST_CLONE_DIR]"
+USAGE="$0 <cve_id> [write_to_outfile=$WRITE_TO_OUTFILE] [outfile_prefix=$SCRIPT_DIR/$OUTFILE_PREFIX] [cvelist_clone_dir=$CVELIST_CLONE_DIR] [exploitdb_clone_dir=$EXPLOITDB_CLONE_DIR] [pocs_in_github_clone_dir=$POCS_IN_GITHUB_CLONE_DIR]"
 if [ $# -lt 1 ]; then
     echo "[-] $USAGE"
     exit 1
@@ -17,6 +21,8 @@ cve_id="${1}"
 write_to_outfile=${2:-"$WRITE_TO_OUTFILE"}
 outfile_prefix=${3:-"$SCRIPT_DIR/$OUTFILE_PREFIX"}
 cvelist_clone_dir=${4:-"$CVELIST_CLONE_DIR"}
+exploitdb_clone_dir=${5:-"$EXPLOITDB_CLONE_DIR"}
+pocs_in_github_clone_dir=${6:-"$POCS_IN_GITHUB_CLONE_DIR"}
 
 # Convert to upper case
 cve_id=$(echo "$cve_id" | tr '[:lower:]' '[:upper:]')
@@ -29,7 +35,7 @@ fi
 
 if [ ! -d "$cvelist_clone_dir" ]; then
     # Cloning CVE List from URL: $CVELIST_URL to $cvelist_clone_dir
-    git clone $CVELIST_URL $CVELIST_CLONE_DIR
+    git clone $CVELIST_URL $cvelist_clone_dir
     if [ $? -ne 0 ]; then
         echo "[*] Cloning of CVE List info failed..."
         exit 1
@@ -44,6 +50,41 @@ fi
 
 # Lookup CVE info for the given CVE ID in CVEList
 cvelist_cve_id_path=$(find "$cvelist_clone_dir" -ipath "*$cve_id*" -type f)
+
+
+if [ ! -d "$pocs_in_github_clone_dir" ]; then
+    # Cloning CVE List from URL: $CVELIST_URL to $cvelist_clone_dir
+    git clone $POCS_IN_GITHUB_CLONE_URL $pocs_in_github_clone_dir
+    if [ $? -ne 0 ]; then
+        echo "[*] Cloning of POCs in Github info failed..."
+        exit 1
+    fi
+else
+    # Pull latest information about CVEs and only show errors in pulling info
+    cwd=$(pwd)
+    cd "$pocs_in_github_clone_dir"
+    git pull 1>/dev/null
+    cd "$cwd"
+fi
+
+# Lookup exploits in github 'POCs in Github' repository
+pocs_in_github_cve_info=$(grep -r -n -i --color "$cve_id" "$pocs_in_github_clone_dir"/README.md)
+
+if [ ! -d "$exploitdb_clone_dir" ]; then
+    # Cloning POCs in ExploitDB
+    git clone $EXPLOITDB_CLONE_URL $exploitdb_clone_dir
+    if [ $? -ne 0 ]; then
+        echo "[*] Cloning of ExploitDB repo failed..."
+        exit 1
+    fi
+else
+    # Pull latest information about CVEs and only show errors in pulling info
+    cwd=$(pwd)
+    cd "$exploitdb_clone_dir"
+    git pull origin main 1>/dev/null
+    cd "$cwd"
+fi
+exploitdb_cve_info=$(grep -r -n -i --color "$cve_id" "$exploitdb_clone_dir"/files_exploits.csv)
 
 # Get CISA's known vulnerabilities list locally
 current_date=$(date +"%Y-%m-%d")
@@ -72,3 +113,10 @@ echo "[+] EPSS Info:" | tee -a "$outfile_path"
 echo "$epss_cve_id_info" | jq -r "." | tee -a "$outfile_path"
 echo -e "\n------\n\n" | tee -a "$outfile_path"
 
+echo "[+] ExploitDB:" | tee -a "$outfile_path"
+echo "$exploitdb_cve_info" | tee -a "$outfile_path"
+echo -e "\n------\n\n" | tee -a "$outfile_path"
+
+echo "[+] POCs In Github Info:" | tee -a "$outfile_path"
+echo "$pocs_in_github_cve_info" | tee -a "$outfile_path"
+echo -e "\n------\n\n" | tee -a "$outfile_path"
